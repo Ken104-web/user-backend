77a42547d1cd0e21d8acc3bc51f4112cfe9f0901
KenCorp <kennethwarui35@gmail.com>
2024-01-11 14:03:46 +0300

    plz run

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,26 @@ import express from "express"
 import { graphqlHTTP } from "express-graphql"
 
 import { UsersResolver } from "./users/resolvers"
+
+async function  main() {
+    const schema = await buildSchema({
+        resolvers: [UsersResolver],
+        emitSchemaFile: true,
+    })
+
+    const app = express()
+
+    app.use(
+        "/graphql",
+        graphqlHTTP({
+            schema: schema,
+            graphiql: true
+        })
+    )
+
+    app.listen(8000)
+
+    console.log("Plz bro run server at http://localhost:8000/graphql")
+    
+}
+main()
